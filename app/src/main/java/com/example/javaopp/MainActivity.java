5663f913bfbcc02213e9a4466ee812f7029eeb29
aziz <19atabaev.aziz@gmail.com>
2020-05-21 21:59:33 +0500

    javaOPP Logcat/info/talk/no filters Anonymous class

--- app/src/main/java/com/example/javaopp/MainActivity.java
+++ app/src/main/java/com/example/javaopp/MainActivity.java
@@ -2,6 +2,9 @@ package com.example.javaopp;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,10 +12,23 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Button button = findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
 
-        Dog dog1 = new Dog();
-        dog1.catchCat(1);
-        dog1.catchCat(3);
+            }
+        });
 
+        Dog singingDog = new Dog() {
+            @Override
+            public void talk() {
+                Log.i("talk", "I'm sining Who let the dogs out");
+            }
+        };
+        singingDog.talk();
+
+        Dog dog = new Dog();
+        dog.talk();
     }
 }
